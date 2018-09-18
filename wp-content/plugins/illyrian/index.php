e88acf7efad201d3acae49ef7a74239040b2da2c
Jurgen <jurgen-kruja@live.com>
2018-09-18 20:48:23 +0200

    Ndryshim Versioni ne 1.3

--- wp-content/plugins/illyrian/index.php
+++ wp-content/plugins/illyrian/index.php
@@ -5,7 +5,7 @@
     Description: ClickJacking plugin in every element you want.
     Author: Jurgen Kruja
     Author URI: https://jkruja.com/
-    Version: 1.2.1
+    Version: 1.3
 */
 
 require_once 'constants.php';
